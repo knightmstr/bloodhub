8d6c69ee7b517ba46ca7e42df4f4f769181aab7d
Sudip32 <sppoudel32@gmail.com>
2018-10-08 12:00:09 +0930

    Deleted previous files (old version).

--- BloodApp/app/src/main/java/com/dipak/dev/bloodapp/SharedPref.java
+++
@@ -1,9 +0,0 @@
-package com.dipak.dev.bloodapp;
-
-public class SharedPref {
-    //Sharedpreference Keys
-    //Name of SharedPreference
-    public static final String SHARED_PREFERENCE_NAME2 = "BloodHub";
-    //public static final String SHARED_PREFERENCE_UID2 = "UID";
-    public static final String SHARED_PREFERENCE_PID2 = "PID";
-}
